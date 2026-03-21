6aeb102a627619ccbe3588f05a111dbe906c29bd
Pahuldeep Singh <pahuldeepsingh12@gmail.com>
2026-03-21 10:12:48 -0500

    fix: handle migrate version error and address coderabbit review

--- libs/migrate/migrate.go
+++ libs/migrate/migrate.go
@@ -5,7 +5,6 @@ import (
     "fmt"
     "io/fs"
     "log"
-
     "github.com/golang-migrate/migrate/v4"
     _ "github.com/golang-migrate/migrate/v4/database/postgres"
     "github.com/golang-migrate/migrate/v4/source/iofs"
@@ -16,18 +15,19 @@ func Up(dsn string, migrations fs.FS, dbName string) error {
     if err != nil {
         return fmt.Errorf("migrate source: %w", err)
     }
-
     m, err := migrate.NewWithSourceInstance("iofs", source, dsn)
     if err != nil {
         return fmt.Errorf("migrate init: %w", err)
     }
     defer m.Close()
-
     if err := m.Up(); err != nil && !errors.Is(err, migrate.ErrNoChange) {
         return fmt.Errorf("migrate up: %w", err)
     }
-
-    version, _, _ := m.Version()
-    log.Printf("migrations up to date at version %d", version)
+    version, _, err := m.Version()
+    if err != nil {
+        log.Printf("warning: could not retrieve migration version: %v", err)
+    } else {
+        log.Printf("migrations up to date at version %d", version)
+    }
     return nil
 }
